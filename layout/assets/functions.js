77dc9b6e79306e6ab9aecc5f595f4ce31af2c0ca
Heinrick Senna <marcelohsenna@protonmail.com>
2026-02-21 18:40:10 -0300

    Updating

---
+++ layout/assets/functions.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  function onScroll() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var elements = document.querySelectorAll('.paralax-image');
+
+    elements.forEach(function (elem) {
+      var img = elem.querySelector('img');
+      if (img) {
+        var offsetTop = elem.getBoundingClientRect().top + scrollTop;
+        img.style.top = (scrollTop - offsetTop) + "px";
+      }
+    });
+  }
+  onScroll();
+  window.addEventListener('scroll', onScroll);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -4678,3 +4678,19 @@ try {
 } catch (error) {
     console.log(error)
 }
+document.addEventListener("DOMContentLoaded", function () {
+  function onScroll() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var elements = document.querySelectorAll('.paralax-image');
+
+    elements.forEach(function (elem) {
+      var img = elem.querySelector('img');
+      if (img) {
+        var offsetTop = elem.getBoundingClientRect().top + scrollTop;
+        img.style.top = (scrollTop - offsetTop) + "px";
+      }
+    });
+  }
+  onScroll();
+  window.addEventListener('scroll', onScroll);
+});
