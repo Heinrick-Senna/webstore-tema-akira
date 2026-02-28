f651979c9d652a4713bf8950e0c6334a47d6a5a6
Heinrick Senna <marcelohsenna@protonmail.com>
2026-02-28 17:17:18 -0300

    Adding checkpoint

--- public/js/script.js
+++ public/js/script.js
@@ -4680,50 +4680,48 @@ try {
 }
 
 try {
-    if (document.querySelector('#HdEtapaLoja').value === 'HOME') {
-        document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
+        if (document.querySelector('#HdEtapaLoja').value == 'HOME') {
             function onScroll() {
-                var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                var elements = document.querySelectorAll('.paralax-image');
+                let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                const elements = document.querySelectorAll('.paralax-image');
 
                 elements.forEach(function (elem) {
-                    var img = elem.querySelector('img');
+                    const img = elem.querySelector('img');
                     if (img) {
-                        var offsetTop = elem.getBoundingClientRect().top + scrollTop;
+                        const offsetTop = elem.getBoundingClientRect().top + scrollTop;
                         img.style.top = (scrollTop - offsetTop) + "px";
                     }
                 });
             }
             onScroll();
             window.addEventListener('scroll', onScroll);
-        });
-    }
+        }
+    });
 } catch (err) {
 
 }
-let functToCall = () => {
-    console.log('teste2!!!')
-    const holder = document.querySelector('[data-wsjs-infos-v2="institutional"]');
-
-    const list = document.createElement('ul');
-    list.className = holder.className
-    list.classList.add('institutional-footer')
-
-    document.querySelectorAll('footer .institutional-footer li a').forEach(elm => {
-        if (elm.title.indexOf('Produto: ') != -1) {
-            list.append(elm)
-        }
-    })
-
-    holder.parentNode.append(list)
-    holder.parentNode.removeAttribute('data-wsjs-container')
-    holder.remove();
-};
 
 window.addEventListener('load', () => {
-    console.log('TESTE!')
     try {
-        isReady('info-lojas', functToCall)
+        isReady('info-lojas', () => {
+            const holder = document.querySelector('[data-wsjs-infos-v2="institutional"]');
+            const list = document.createElement('ul');
+            list.className = holder.className;
+            list.classList.add('institutional-footer');
+
+            document.querySelectorAll('footer .institutional-footer li a').forEach(elm => {
+                if (elm.title.indexOf('Produto: ') != -1) {
+                    elm.title = elm.title.replace('Produto: ', '');
+                    elm.innerText = elm.title;
+                    list.append(elm);
+                }
+            });
+
+            holder.parentNode.append(list);
+            holder.parentNode.removeAttribute('data-wsjs-container');
+            holder.remove();
+        });
     } catch (err) {
 
     }
